83b5ef72b301128470e292aa63e9d64ce42e8f39
nhussein2026 <naser738nas@gmail.com>
2025-09-11 19:42:12 +0300

    refactor: remove unnecessary React imports from Navbar and ProtectedRoute components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../redux/store";
