1a8b55b9fbbccff2cb9fc7f3b97ea991c3d80012
nhussein2026 <naser738nas@gmail.com>
2025-09-11 19:39:55 +0300

    refactor: remove unnecessary React imports from multiple components

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { Toaster, toast } from "sonner";
--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Card, CardContent } from "../components/ui/card";
 import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
