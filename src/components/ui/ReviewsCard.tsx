2fa2ed53ac5cf451df38fa717e62ce6a23c2b780
Abdulrahman Omar <109946184+aaomerali@users.noreply.github.com>
2025-10-11 16:02:59 +0300

    building reviews section

---
+++ src/components/ui/ReviewsCard.tsx
@@ -0,0 +1,33 @@
+import { FaStar } from "react-icons/fa";
+
+type Review = {
+  name: string;
+  rating: number;
+  comment: string;
+  image: string;
+};
+
+export function ReviewsCard({ item }: { item: Review }) {
+  return (
+    <div className="bg-[#F4E8EC] w-full sm:w-[45%] lg:w-[30%] max-w-md rounded-2xl shadow-md hover:shadow-lg p-6 flex flex-col justify-between transition text-right">
+      <p className="text-gray-700 text-sm mb-6 leading-relaxed">
+        {item.comment}
+      </p>
+
+      <div className="flex items-center justify-end gap-3">
+        <div className="flex flex-col items-end">
+          <span className="text-[#7b1d25] font-semibold">{item.name}</span>
+          <div className="flex flex-row-reverse">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <FaStar
+                key={star}
+                className={`text-xl ${star <= item.rating ? "text-[#1B8D6B]" : "text-gray-300"}`}
+              />
+            ))}
+          </div>
+        </div>
+        <img src={item.image} alt="profile" className="w-10 h-10 rounded-full" />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import Header from '../components/homePage/Header';
+import AboutSection from "../components/homePage/ِAboutSection";
+import Header from "../components/homePage/Header";
+import ServicesSection from "../components/homePage/ServicesSection";
+import TestimonialsSection from "..//components/homePage/TestimonialsSection";
+
 
 export default function Home() {
   return (
     <div className="w-full">
       <Header />
+      <AboutSection />
+      <ServicesSection />
+      <div className="w-full flex  justify-center"><hr className="w-[80%] text-gray-300" /></div>
+      <TestimonialsSection />
     </div>
   );
 }
